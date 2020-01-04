6e56ad8b2e4871d6589a1c5f581b64635741a659
Valery Proshchenkov <vm.proshchenkov@gmail.com>
2020-01-04 20:27:05 +0300

    Add files via upload

--- unit1.pas
+++ unit1.pas
@@ -6,7 +6,7 @@ interface
 
 uses
   Classes, SysUtils, Forms, Controls, Graphics, Dialogs, ExtCtrls, Menus,
-  StdCtrls, ComCtrls, ExtDlgs, Buttons;
+  StdCtrls, ComCtrls, ExtDlgs, Spin;
 
 type
 
@@ -14,7 +14,7 @@ type
 
   TForm1 = class(TForm)
     ColorDialog1: TColorDialog;
-    ComboBox1: TComboBox;
+    Image1: TImage;
     MainMenu1: TMainMenu;
     MenuItem1: TMenuItem;
     MenuItem2: TMenuItem;
@@ -22,26 +22,24 @@ type
     MenuItem4: TMenuItem;
     MenuItem5: TMenuItem;
     OpenPictureDialog1: TOpenPictureDialog;
-    PaintBox1: TPaintBox;
     Panel1: TPanel;
     Panel2: TPanel;
     SavePictureDialog1: TSavePictureDialog;
+    ScrollBox1: TScrollBox;
+    SpinEdit1: TSpinEdit;
     StatusBar1: TStatusBar;
-    TaskDialog1: TTaskDialog;
-    procedure ComboBox1Change(Sender: TObject);
     procedure FormCreate(Sender: TObject);
-    procedure FormResize(Sender: TObject);
     procedure MenuItem2Click(Sender: TObject);
     procedure MenuItem3Click(Sender: TObject);
     procedure MenuItem5Click(Sender: TObject);
-    procedure PaintBox1MouseDown(Sender: TObject; Button: TMouseButton;
+    procedure Image1MouseDown(Sender: TObject; Button: TMouseButton;
       Shift: TShiftState; X, Y: integer);
-    procedure PaintBox1MouseLeave(Sender: TObject);
-    procedure PaintBox1MouseMove(Sender: TObject; Shift: TShiftState;
-      X, Y: integer);
-    procedure PaintBox1MouseUp(Sender: TObject; Button: TMouseButton;
+    procedure Image1MouseLeave(Sender: TObject);
+    procedure Image1MouseMove(Sender: TObject; Shift: TShiftState; X, Y: integer);
+    procedure Image1MouseUp(Sender: TObject; Button: TMouseButton;
       Shift: TShiftState; X, Y: integer);
     procedure Panel2Click(Sender: TObject);
+    procedure SpinEdit1Change(Sender: TObject);
   private
 
   public
@@ -51,8 +49,6 @@ type
 var
   Form1: TForm1;
   draw: boolean;
-  bmp: TBitmap;
-  x0, y0: integer;
 
 implementation
 
@@ -60,39 +56,33 @@ implementation
 
 { TForm1 }
 
-procedure TForm1.PaintBox1MouseDown(Sender: TObject; Button: TMouseButton;
+procedure TForm1.Image1MouseDown(Sender: TObject; Button: TMouseButton;
   Shift: TShiftState; X, Y: integer);
 begin
   draw := True;
-  PaintBox1.Canvas.MoveTo(x, y);
-  PaintBox1.Canvas.Pixels[x, y] := ColorDialog1.Color;
-  x0 := x;
-  y0 := y;
-end;
-
-procedure TForm1.FormResize(Sender: TObject);
-begin
-  StatusBar1.Panels[1].Text :=
-    ' ' + PaintBox1.Width.ToString + ' x ' + PaintBox1.Height.ToString + ' пкс';
+  Image1.Canvas.MoveTo(x, y);
 end;
 
 procedure TForm1.FormCreate(Sender: TObject);
 begin
-  bmp := TBitmap.Create;
+  Image1.Width := 800;
+  Image1.Height := 600;
+  Image1.Top := 10;
+  Image1.Left := 10;
+  Image1.Canvas.FillRect(0, 0, Image1.Width, Image1.Height);
 end;
 
-procedure TForm1.ComboBox1Change(Sender: TObject);
+procedure TForm1.SpinEdit1Change(Sender: TObject);
 begin
-  PaintBox1.Canvas.Pen.Width := ComboBox1.ItemIndex + 1;
+  Image1.Canvas.Pen.Width := SpinEdit1.Value;
 end;
 
 procedure TForm1.MenuItem2Click(Sender: TObject);
 begin
   if OpenPictureDialog1.Execute then
   begin
-    bmp.LoadFromFile(OpenPictureDialog1.FileName);
-    PaintBox1.Canvas.Draw(0, 0, bmp);
-    bmp.Free;
+    Image1.Picture.LoadFromFile(OpenPictureDialog1.FileName);
+    StatusBar1.Panels[1].Text := OpenPictureDialog1.FileName;
   end;
 end;
 
@@ -100,10 +90,8 @@ procedure TForm1.MenuItem3Click(Sender: TObject);
 begin
   if SavePictureDialog1.Execute then
   begin
-    bmp.Canvas.CopyRect(Rect(0, 0, PaintBox1.Width, PaintBox1.Height),
-      PaintBox1.Canvas, Rect(0, 0, PaintBox1.Width, PaintBox1.Height));
-    bmp.SaveToFile(SavePictureDialog1.FileName);
-    bmp.Free;
+    Image1.Picture.SaveToFile(SavePictureDialog1.FileName);
+    StatusBar1.Panels[1].Text := SavePictureDialog1.FileName;
   end;
 end;
 
@@ -121,21 +109,21 @@ begin
   end;
 end;
 
-procedure TForm1.PaintBox1MouseLeave(Sender: TObject);
+procedure TForm1.Image1MouseLeave(Sender: TObject);
 begin
   StatusBar1.Panels[0].Text := '';
 end;
 
-procedure TForm1.PaintBox1MouseMove(Sender: TObject; Shift: TShiftState; X, Y: integer);
+procedure TForm1.Image1MouseMove(Sender: TObject; Shift: TShiftState; X, Y: integer);
 begin
   if draw then
   begin
-    PaintBox1.Canvas.LineTo(x, y);
+    Image1.Canvas.LineTo(x, y);
   end;
   StatusBar1.Panels[0].Text := ' ' + X.ToString + ', ' + Y.ToString;
 end;
 
-procedure TForm1.PaintBox1MouseUp(Sender: TObject; Button: TMouseButton;
+procedure TForm1.Image1MouseUp(Sender: TObject; Button: TMouseButton;
   Shift: TShiftState; X, Y: integer);
 begin
   draw := False;
@@ -145,7 +133,7 @@ procedure TForm1.Panel2Click(Sender: TObject);
 begin
   if ColorDialog1.Execute then
   begin
-    PaintBox1.Canvas.Pen.Color := ColorDialog1.Color;
+    Image1.Canvas.Pen.Color := ColorDialog1.Color;
     Panel2.Color := ColorDialog1.Color;
   end;
 end;
